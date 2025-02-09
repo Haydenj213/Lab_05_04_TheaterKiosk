de0262a4fe81c29aa6a51eb29629a22a71bb8d29
Haydenj213 <johns3hm@mail.uc.edu>
2025-02-09 17:33:27 -0500

    Final Code Added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Hello Welcome to Trinity Theaters! Please enter your age below.");
+
+        int age = in.nextInt();
+
+        if (age >= 21) {
+            System.out.println("Thank you for entering your age. You are at least 21 years old. You receive a wristband letting us know that you are.");
+        }
     }
 }
